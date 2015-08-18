3124c6d6e87fe518f2175abfb04f9fc83ae35a40
Sus Oliver <osus@oflxs337.dwd.de>
2015-08-18 08:25:31 +0000

    added 3 different comment types

--- InputOutput.java
+++ InputOutput.java
@@ -1,10 +1,20 @@
-// Return rectangle area given user input values //
+// Return rectangle area given user input values
 
 import java.util.Scanner;
 
+/**
+ * Calculates area of rectangle.
+ * Reads length L and width W from user input
+ * and outputs calculated area A.
+ * @author ignores command line arguments
+ * @since JDK1.0
+ * @see java.util.Scanner
+ */
 public class InputOutput
 {
     public static void main(String[] args){
+
+	// Declaration of some integer variables.	
 	int L, W, A;
 
 	Scanner inputvalue = new Scanner(System.in);
@@ -14,6 +24,11 @@ public class InputOutput
 	System.out.print("Enter width W: ");
 	W = inputvalue.nextInt();
 	A = L * W;
+
+	/*
+	 * Output of calculated area.
+	 * Area will be given in cm2 in command line.	  
+	 */
 	System.out.println("The rectangle's area is: " + A + " cm^2");
 
     }
