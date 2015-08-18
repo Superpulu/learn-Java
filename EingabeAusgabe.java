89f236ee3fe91890e1c88f48c0c22342665d9ed6
Sus Oliver <osus@oflxs337.dwd.de>
2015-08-18 07:47:17 +0000

    now finally adding EingabeAusgabe.java

---
+++ EingabeAusgabe.java
@@ -0,0 +1,11 @@
+// Return rectangle area given user input values //
+
+import java.util.Scanner;
+
+public class EingabeAusgabe
+{
+    public static void main(String[] args){
+	System.out.println("Enter length l:");
+	System.out.println("Enter width w:");
+    }
+}
