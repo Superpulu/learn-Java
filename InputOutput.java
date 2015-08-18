2b93d4308aa511ca8304f93e2b25cfa6b5222380
Oliver Sus <osus79@gmail.com>
2015-08-18 10:07:42 +0200

    renamed java source file

---
+++ InputOutput.java
@@ -0,0 +1,15 @@
+// Return rectangle area given user input values //
+
+import java.util.Scanner;
+
+public class InputOutput
+{
+    public static void main(String[] args){
+	int L, W, A;
+
+	Scanner inputvalue = new Scanner(System.in);
+
+	System.out.println("Enter length L:");
+	System.out.println("Enter width W:");
+    }
+}
