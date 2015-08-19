32ffe4cf3c69403edac319083e991bb90fdc7b65
Oliver Sus <osus79@gmail.com>
2015-08-19 16:17:32 +0200

    filled array with default values; added printing of all array values at once with Arrays.toString

--- ArrayDeclarationMultidimensional.java
+++ ArrayDeclarationMultidimensional.java
@@ -3,15 +3,21 @@ import java.util.Arrays;
 public class ArrayDeclarationMultidimensional{
     
     public static void main(String[] args) {
+	int fill = -999;
 	System.out.println("Declaration of multidimensional arrays:");
-	int[][][] a = new int[2][1][2];
-	int[][] b = { {1}, {1,2,1}, {1,2,3,2,1}, {1,2,3,4,3,2,1},
+	int[] a = new int[10];
+	Arrays.fill(a, fill); 
+	System.out.println("a = " + Arrays.toString(a));
+	int[][][] b = new int[2][3][2];
+	System.out.println("b = " + Arrays.deepToString(b));
+	int[][] c = { {1}, {1,2,1}, {1,2,3,2,1}, {1,2,3,4,3,2,1},
                       {1,2,3,2,1}, {1,2,1}, {1} };
+	System.out.println("c = " + Arrays.deepToString(c));
 
-        System.out.println("\nNichtrechteckiger Array b:");
-        for (int i=0; i<b.length; i++) {
-	    for (int j=0; j<b[i].length; j++) {
-		System.out.print(b[i][j]);
+        System.out.println("\nNichtrechteckiger Array c:");
+        for (int i=0; i<c.length; i++) {
+	    for (int j=0; j<c[i].length; j++) {
+		System.out.print(c[i][j]);
 	    }
 	    System.out.println();
         }
