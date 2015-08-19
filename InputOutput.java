c75b7c005320fc62f102a19071ea88fd6dc7455c
Oliver Sus <osus79@gmail.com>
2015-08-19 11:11:04 +0200

    added REFERENCE variables

--- InputOutput.java
+++ InputOutput.java
@@ -1,6 +1,7 @@
 // Return rectangle area given user input values
 
 import java.util.Scanner;
+import java.util.Date;
 
 /**
  * Calculates area of rectangle.
@@ -14,15 +15,21 @@ public class InputOutput
 {
     public static void main(String[] args){
 
-	// Declaration of some variables of various types.	
+	// Declaration of some variables of various PRIMITIVE types.	
 	int L, W, A;
 	float halfArea;
 	float realNumber = 3.01f;
 	boolean isBooked;
 	char signQ = '\u0051';
-	long smallNumber = -92234645126L;
-	String someText = "Der VfB wird Meister.";	
+	long smallNumber = -9223464512L;
+	String someText = "Der VfB wird Meister";	
+
+	// Declaration of REFERENCE variables
+	Date a = new Date(45341200000L);
+	Date b = a;
 	Scanner inputvalue = new Scanner(System.in);
+
+	System.out.println("\na = " + a + " b = " + b);
 
 	isBooked = true;
 
@@ -42,11 +49,11 @@ public class InputOutput
 	System.out.println("and half the area is: " + halfArea+ " cm^2");
 
 	// Now print above variable types for testing
-	System.out.println("\nInsert empty line!");
+	System.out.println("\nInsert empty line before this text!");
 	System.out.println("isBooked = " + isBooked);
 	System.out.println("signQ = " + signQ + ", smallNumber = " + 
 			   smallNumber + ", someText = " + someText
-			   + ", realNumber= " + realNumber);
+			   + ", realNumber = " + realNumber);
 	// ...applying a foor loop
 	System.out.println("\nWithin for loop:\n");
 	for (int i = 10; i > 5; i--){
