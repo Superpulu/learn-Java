ed2a791f1dcb47ac1996b7680676261fd0fb1027
Oliver Sus <osus79@gmail.com>
2015-08-27 16:18:53 +0200

    n/a

--- ConditionalsAndBooleans.java
+++ ConditionalsAndBooleans.java
@@ -1,12 +1,9 @@
 public class ConditionalsAndBooleans{
     public static void main(String[] args){
 	int a = 4;
-	if (a == 4){
-	    System.out.println("a == 4");
-	}
-
 	int b = 5;
 	boolean result;
+
 	// boolean operators:
 	result = a < b;
 	System.out.println(result);
@@ -26,5 +23,34 @@ public class ConditionalsAndBooleans{
 	System.out.println(result);
 	result = !result; // logical not
 	System.out.println(result);
+
+	// if 
+	if (a == b){
+	    System.out.println("a == b");
+	}	
+	
+	// if - else
+	if (a==b){
+	    System.out.println("a == b");
+	} else {
+	    System.out.println("a != b");
+	}
+	// corresponding one-liner without {}, but not recommended
+	if (a==b) System.out.println("a == b"); else System.out.println("a != b");
+	// even shorter
+	int resultInteger = a == 5 ? 1 : 8;
+	System.out.println("result = " + resultInteger);
+
+	String one = new String("VfB");
+	String two = new String("VfB");
+	String three = one;
+
+	boolean oneTwo = one == two;
+	boolean oneTwoEquals = one.equals(two);
+	boolean oneThree = one == three;
+	System.out.println(oneTwo);
+	System.out.println(oneTwoEquals);
+	System.out.println(oneThree);
+
     }
 }
