c4f6cb81c54e1479ba8de003ac246454215a9408
Oliver Sus <osus79@gmail.com>
2015-09-08 17:18:11 +0200

    adding exception handling tutorial file

---
+++ Exception.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+public class Exception{
+
+    public static void findFile(File f){
+	System.out.println("file exists? "+ f.isFile());
+    }
+
+    public static void main(String[] args){
+	File file1 = new File("/home/oliver/coding/learn-java/Exception.java");
+	File file2 = new File("/home/oliver/coding/learn-java/Exception.jav");
+	findFile(file1);
+	findFile(file2);
+    }
+    
+}
